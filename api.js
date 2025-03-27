54122d819d134074b02c1bcf4780afaea8ad2f1a
mallikanicole <malmichael23@gmail.com>
2025-03-27 15:39:53 -0400

    part2 use .fetch() .then()

---
+++ api.js
@@ -0,0 +1,14 @@
+const BASE_URL='https://jsonplaceholder.typicode.com/posts';
+export function getPosts(){
+    return fetch(BASE_URL)
+    .then(response=>{
+        if (!response.ok){
+            throw new Error(`HTTP error:${response.status}`);
+        }
+        return response.json();
+    })
+    .catch(error=>{
+        console.error('Fetch Failed:',error.message);
+        throw error;
+    });
+}
